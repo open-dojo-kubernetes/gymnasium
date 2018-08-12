80926405478a235d6b192e815d44a13adfecb412
lPegz <lhp.battousai@gmail.com>
2018-08-12 18:37:57 -0300

    Fixed guid class

--- src/app/model/guid.ts
+++ src/app/model/guid.ts
@@ -1,9 +1,9 @@
-import Math from 'Math';
+declare var Math;
 
 export class Guid {
-/* tslint disable:no-bitwise */
   static newGuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      // tslint:disable-next-line:no-bitwise
       const randomNumber = Math.random() << 4 | 0, v = c === 'x' ? randomNumber : (randomNumber & 0x3 | 0x8);
       return v.toString(16);
     });
